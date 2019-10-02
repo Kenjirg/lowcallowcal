28cf17be3784d57f4430fe0abd36f6c6a6953b9b
Kenji <imkenjiasia@gmail.com>
2019-10-02 16:58:18 +0800

    pchse frames

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,7 +6,20 @@ class PurchasesController < ApplicationController
 	def new
 		@purchase = Purchase.new
 	end
+	def create
+		@purchase=current_user.purchases.build purchase_params
+		if @purchase.save
+			redirect_to purchases_path
+		else
+			render 'new'
+		end
+	end
 	def show
-		@purchase = Purchase.find params[:id]
+		@purchase=Purchase.find params[:id]
+		@order=Order.find_by(user: current_user, purchase: @purchase) || Order.new
+	end
+	private
+	def purchase_params
+		params.require(:purchase).permit(:time_period, :meal_id, :end_time)
 	end
 end
